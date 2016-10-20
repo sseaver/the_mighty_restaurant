8c481e66c3894e52b6dcc6ed667cdc9e9e4c23fd
sseaver <sseaver321@gmail.com>
2016-10-20 14:58:20 -0400

    index page with login form

--- the_mighty_restaurant/app/views.py
+++ the_mighty_restaurant/app/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render
 from app.models import Menu, Order, Profile, Table
 from django.views.generic import CreateView, TemplateView
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 
 
 class IndexView(TemplateView):
     template_name = "index.html"
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context['login_form'] = AuthenticationForm
+        return context
 
 
 class UserCreateView(CreateView):
