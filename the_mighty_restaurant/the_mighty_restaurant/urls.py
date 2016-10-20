b2e362d2ac29ef301c451fef76295c3478ffcf72
ConnorFieldUser <conquest.field@gmail.com>
2016-10-20 14:06:46 -0400

    Added templates

--- the_mighty_restaurant/the_mighty_restaurant/urls.py
+++ the_mighty_restaurant/the_mighty_restaurant/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from menu_app.views import IndexView, UserCreateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('django.contrib.auth.urls')),
+    url(r'^$', IndexView.as_view(), name="index_view"),
+    url(r'^create_user/$', UserCreateView.as_view(), name="user_create_view")
 ]
