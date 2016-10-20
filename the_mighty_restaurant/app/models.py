2c4a5f2c69167d8cddf16f11ac6159c276385ddf
sseaver <sseaver321@gmail.com>
2016-10-20 13:32:02 -0400

    created models

--- the_mighty_restaurant/app/models.py
+++ the_mighty_restaurant/app/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+ACCESS_LEVELS = [
+    ('s', 'Server'),
+    ('o', 'Owner'),
+    ('c', 'Chef'),
+    ('g', 'Guest')
+]
+
+
+class Profile(models.Model):
+    user = models.OneToOneField('auth.User')
+    access_level = models.CharField(max_length=1, choices=ACCESS_LEVELS)
+
+    @property
+    def is_owner(self):
+        return self.access_level == 'o'
+
+
+class Menu(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+    drink = models.CharField(max_length=20)
+
+
+class Order(models.Model):
+    item = models.OneToManyField(Menu)
+    notes = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class Table(models.Model):
+    order = models.ForeignKey(Order)
+    table_number = models.IntegerField()
+    seat = models.CharField(max_length=2)
