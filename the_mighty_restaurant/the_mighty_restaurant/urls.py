cf9af0f4edd0ee3e6c11432e1f0cbf5f16cd752a
ConnorFieldUser <conquest.field@gmail.com>
2016-10-21 10:53:13 -0400

    more urls

--- the_mighty_restaurant/the_mighty_restaurant/urls.py
+++ the_mighty_restaurant/the_mighty_restaurant/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from app.views import IndexView, UserCreateView, ProfileView
+from app.views import IndexView, UserCreateView, ProfileView, OrderCreateView, OrderUpdateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('django.contrib.auth.urls')),
     url(r'^$', IndexView.as_view(), name="index_view"),
     url(r'^create_user/$', UserCreateView.as_view(), name="user_create_view"),
-    url(r'^account/profile/$', ProfileView.as_view(), name="profile_view")
+    url(r'^account/profile/$', ProfileView.as_view(), name="profile_view"),
+    url(r'^create_order/$', OrderCreateView, name="order_create_view"),
+    url(r'^update_order/$', OrderUpdateView, name="order_update_view")
 ]
