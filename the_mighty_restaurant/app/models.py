e033f968b487272718f34356d5ceadd4de80afee
ConnorFieldUser <conquest.field@gmail.com>
2016-10-23 23:19:39 -0400

    commit

--- the_mighty_restaurant/app/models.py
+++ the_mighty_restaurant/app/models.py
@@ -90,7 +90,7 @@ class Order(models.Model):
     drink = models.CharField(max_length=20)
     notes = models.TextField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
-    completed = models.BooleanField(default=False, choices=BOOL_CHOICES)
+    completed = models.BooleanField(choices=BOOL_CHOICES, default=False)
 
     def __str__(self):
         return str(self.item)
